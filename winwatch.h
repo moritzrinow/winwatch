e716e7043645f87f5d66b29bff29c3d8bd773448
Moritz Rinow <m.rinow@topmotive.eu>
2019-12-30 23:47:11 +0100

    added winwatch file

---
+++ winwatch.h
@@ -0,0 +1,94 @@
+#ifndef _WINWATCH_H
+#define _WINWATCH_H
+
+#include <Windows.h>
+
+#define WINWATCH_STATE_RUNNING 1
+#define WINWATCH_STATE_PAUSED 2
+#define WINWATCH_STATE_STOPPED 3
+
+typedef struct WinWatch {
+  LARGE_INTEGER Frequency;
+  LARGE_INTEGER StartTime;
+  LARGE_INTEGER TimePaused;
+  LARGE_INTEGER PauseTime;
+  LARGE_INTEGER StopTime;
+  int           State;
+} WinWatch;
+
+#define StartWinWatch() StartWinWatchA() // Creates and starts a new winwatch
+#define StopWinWatch(w) StopWinWatchA(&w) // Stops a winwatch, which disables pausing or resuming the watch instance in the future
+#define PauseWinWatch(w) PauseWinWatchA(&w) // Pauses a winwatch. The time between now and the next resume will be measured and subtracted from the final measurement
+#define ResumeWinWatch(w) ResumeWinWatchA(&w) // Resumes a winwatch
+
+#define WinWatchElapsedTicks(w) (w.StopTime.QuadPart - w.StartTime.QuadPart - w.TimePaused.QuadPart) // Get the elapsed ticks as 64bit signed integer
+#define WinWatchElapsedMicro(w) (WinWatchElapsedTicks(w) * 1000000 / w.Frequency.QuadPart) // Get the elapsed microseconds as 64 signed integer
+#define WinWatchElapsedMilli(w) ((double)WinWatchElapsedMicro(w) / 1000.0) // Get the elapsed milliseconds as 64bit float
+#define WinWatchElapsedSeconds(w) ((double)WinWatchElapsedMicro(w) / 1000000.0) // Get the elapsed seconds as 64bit float
+
+WinWatch StartWinWatchA(
+  void
+)
+{
+  BOOL res;
+  WinWatch watch;
+  watch.Frequency.QuadPart = 0L;
+  watch.StartTime.QuadPart = 0L;
+  watch.TimePaused.QuadPart = 0L;
+  watch.PauseTime.QuadPart = 0L;
+  watch.StopTime.QuadPart = 0L;
+  watch.State = WINWATCH_STATE_RUNNING;
+
+  res = QueryPerformanceFrequency(&watch.Frequency);
+  res = QueryPerformanceCounter(&watch.StartTime);
+
+  return watch;
+}
+
+void StopWinWatchA(
+  WinWatch *w
+)
+{
+  BOOL res;
+
+  if (w->State == WINWATCH_STATE_RUNNING) {
+    res = QueryPerformanceCounter(&w->StopTime);
+  }
+  else if (w->State == WINWATCH_STATE_PAUSED) {
+    w->StopTime = w->PauseTime;
+  }
+
+  w->State = WINWATCH_STATE_STOPPED;
+}
+
+void PauseWinWatchA(
+  WinWatch *w
+)
+{
+  BOOL res;
+
+  if (w->State == WINWATCH_STATE_RUNNING) {
+    res = QueryPerformanceCounter(&w->PauseTime);
+  }
+
+  w->State = WINWATCH_STATE_PAUSED;
+}
+
+void ResumeWinWatchA(
+  WinWatch *w
+)
+{
+  BOOL res;
+  LARGE_INTEGER time;
+  res = QueryPerformanceCounter(&time);
+
+  if (w->State == WINWATCH_STATE_PAUSED) {
+    LARGE_INTEGER timePaused;
+    timePaused.QuadPart = time.QuadPart - w->PauseTime.QuadPart;
+    w->TimePaused.QuadPart += timePaused.QuadPart;
+  }
+
+  w->State = WINWATCH_STATE_RUNNING;
+}
+
+#endif //_WINWATCH_H
